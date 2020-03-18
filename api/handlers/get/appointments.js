bb6413288b1a6e96cc3a700d26173f82ddbce6c9
YoloGamer123415 <36742291+YoloGamer123415@users.noreply.github.com>
2020-03-18 13:57:45 +0100

    Add files via upload

--- api/handlers/get/appointments.js
+++ api/handlers/get/appointments.js
@@ -1,5 +1,7 @@
-import Handler from "../../handler";
-import Agenda from "../../Google/Agenda";
+import Handler from '../../handler';
+import Agenda from '../../Google/Agenda';
+
+const ISO_DATE_REGEX = /(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d\.\d+([+-][0-2]\d:[0-5]\d|Z))|(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d([+-][0-2]\d:[0-5]\d|Z))|(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d([+-][0-2]\d:[0-5]\d|Z))/;
 
 /**
  * The handler for the testcase.
@@ -10,13 +12,13 @@ import Agenda from "../../Google/Agenda";
 class FetchAppointmentsHandler extends Handler {
     /**
      * Creates an instance of FetchAppointmentsHandler.
-     * 
+     *
      * @param {import('express').Request} req
      * @param {import('express').Response} res
      * @memberof FetchAppointmentsHandler
      */
     constructor(req, res) {
-        super(req, res)
+        super(req, res);
     }
 
     /**
@@ -27,17 +29,25 @@ class FetchAppointmentsHandler extends Handler {
      */
     run(args) {
         if (this.mayRun) {
-            let agenda = new Agenda()
-    
-            agenda.getAppointments(args[0], args[1]).then(res => {
-                this.send({ appointments: res})
-            })
-            .catch(err => {
-                console.error(err)
-                this.error(err)
-            })
+            let hasCalendarId = args[0] && !ISO_DATE_REGEX.test(args[0]);
+
+            let calendarId = hasCalendarId && args[0];
+            let startDate = hasCalendarId ? args[1] : args[0];
+            let endDate = hasCalendarId ? args[2] : args[1];
+
+            let agenda = new Agenda(calendarId);
+
+            agenda
+                .getAppointments(startDate, endDate)
+                .then(res => {
+                    this.send({ appointments: res });
+                })
+                .catch(err => {
+                    console.error(err);
+                    this.error(err);
+                });
         }
     }
 }
 
-export default FetchAppointmentsHandler
+export default FetchAppointmentsHandler;
