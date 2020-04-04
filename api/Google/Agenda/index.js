f7968b73e1523eb26dd61113709c692e5ef4d5bf
YoloGamer123415 <jorambuitenhuis@gmail.com>
2020-04-04 10:04:47 +0200

    Updated some code

--- api/Google/Agenda/index.js
+++ api/Google/Agenda/index.js
@@ -1,7 +1,7 @@
-import Google from "..";
-import { google } from "googleapis";
-import Appointment from "./Appointment";
-import Calendar from "./Calendar";
+import Google from '..';
+import { google } from 'googleapis';
+import Appointment from './Appointment';
+import Calendar from './Calendar';
 
 /**
  * Get the time today started, eg. jan 1, 2019, 00:00:00.000.
@@ -9,8 +9,8 @@ import Calendar from "./Calendar";
  * @returns {Date}
  */
 function getDayStartTime() {
-    let d = new Date()
-    return new Date(d.getFullYear(), d.getMonth(), d.getDate())
+    let d = new Date();
+    return new Date(d.getFullYear(), d.getMonth(), d.getDate());
 }
 
 /**
@@ -19,26 +19,35 @@ function getDayStartTime() {
  * @returns {Date}
  */
 function getDayEndTime() {
-    let d = new Date()
-    let tomorrow = new Date(d.getFullYear(), d.getMonth(), d.getDate() + 1)
-    return new Date(tomorrow.setSeconds(-1))
+    let d = new Date();
+    let tomorrow = new Date(d.getFullYear(), d.getMonth(), d.getDate() + 1);
+    return new Date(tomorrow.setSeconds(-1));
 }
 
 class Agenda extends Google {
-    constructor() {
-        super()
+    /**
+     * Creates an instance of Agenda.
+     *
+     * @param {String} calendarId
+     * @memberof Agenda
+     */
+    constructor(calendarId) {
+        super();
+
+        this.calendarId = calendarId;
 
         this.agenda = google.calendar({
             version: 'v3',
             auth: this.oAuth2Client
-        })
+        });
     }
 
     getCalendars() {
         return new Promise((resolve, reject) => {
-            this.agenda.calendarList.list({
-                showHidden: true
-            })
+            this.agenda.calendarList
+                .list({
+                    showHidden: true
+                })
                 .then(r => r.data.items)
                 .then(res => {
                     /**
@@ -47,16 +56,18 @@ class Agenda extends Google {
                     let ret = [];
 
                     res.forEach(calendar => {
-                        ret.push(new Calendar({
-                            id: calendar.id,
-                            summary: calendar.summary,
-                            timezone: calendar.timeZone,
-                            color: {
-                                foreground: calendar.foregroundColor,
-                                background: calendar.backgroundColor
-                            },
-                            hidden: !calendar.selected
-                        }));
+                        ret.push(
+                            new Calendar({
+                                id: calendar.id,
+                                summary: calendar.summary,
+                                timezone: calendar.timeZone,
+                                color: {
+                                    foreground: calendar.foregroundColor,
+                                    background: calendar.backgroundColor
+                                },
+                                hidden: !calendar.selected
+                            })
+                        );
                     });
 
                     resolve(ret);
@@ -74,48 +85,60 @@ class Agenda extends Google {
      */
     getAppointments(start, end) {
         return new Promise((resolve, reject) => {
-            start = new Date(start || getDayStartTime())
-            end = new Date(end || getDayEndTime())
-    
+            start = new Date(start || getDayStartTime());
+            end = new Date(end || getDayEndTime());
+
             if (start.getTime() >= end.getTime()) {
-                return reject(`the start time should be before the end time`)
+                return reject(`the start time should be before the end time`);
             }
 
-            this.agenda.colors.get().then(r =>  r.data.event)
+            this.agenda.colors
+                .get()
+                .then(r => r.data.event)
                 .then(colors => {
-                    this.agenda.events.list({ 
-                        calendarId: 'primary',
-                        timeMin: start,
-                        timeMax: end,
-                        singleEvents: true,
-                        orderBy: 'startTime',
-                        prettyPrint: true, // TODO: do we really include this?
-                        showDeleted: false // TODO: do we really exclude this?
-                    }).then(r => r.data.items)
-                        .then(res => {
-                            let ret = []
-            
-                            res.forEach(event => {
-                                ret.push(new Appointment({
-                                    id: event.id,
-                                    title: event.summary,
-                                    description: event.description,
-                                    location: event.location,
-                                    color: colors[event.colorId || 1],
-                                    time: {
-                                        start: new Date(event.start.dateTime || event.start.date),
-                                        end: new Date(event.end.dateTime || event.end.date),
-                                    }
-                                }))
-                            })
-            
-                            resolve(ret)
+                    this.agenda.events
+                        .list({
+                            calendarId: this.calendarId || 'primary',
+                            timeMin: start,
+                            timeMax: end,
+                            singleEvents: true,
+                            orderBy: 'startTime',
+                            prettyPrint: true, // TODO: do we really include this?
+                            showDeleted: false // TODO: do we really exclude this?
                         })
-                        .catch(reject)
+                        .then(r => r.data.items)
+                        .then(res => {
+                            let ret = [];
+
+                            res.forEach(event => {
+                                ret.push(
+                                    new Appointment({
+                                        id: event.id,
+                                        title: event.summary,
+                                        description: event.description,
+                                        location: event.location,
+                                        color: colors[event.colorId || 1],
+                                        time: {
+                                            start: new Date(
+                                                event.start.dateTime ||
+                                                    event.start.date
+                                            ),
+                                            end: new Date(
+                                                event.end.dateTime ||
+                                                    event.end.date
+                                            )
+                                        }
+                                    })
+                                );
+                            });
+
+                            resolve(ret);
+                        })
+                        .catch(reject);
                 })
-                .catch(reject)
-        })
+                .catch(reject);
+        });
     }
 }
 
-export default Agenda
+export default Agenda;
